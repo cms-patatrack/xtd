4813274021355406c28efb8318955d61e9dde97f
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-23 16:45:47 +0200

    Handle NaN and infinity in the comparisons

--- test/common/cpu_test.h
+++ test/common/cpu_test.h
@@ -4,15 +4,16 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+#pragma once
+
 // C++ standard headers
 #include <vector>
 
-// Catch2 headers
-#define CATCH_CONFIG_NO_POSIX_SIGNALS
-#include <catch.hpp>
+// test headers
+#include "compare.h"
 
 template <typename ResultType, typename InputType, ResultType (*XtdFunc)(InputType), ResultType (*StdFunc)(InputType)>
-void test(std::vector<double> const& values) {
+inline void test(std::vector<double> const& values) {
   for (double value : values) {
     // convert the input data to the type to be tested
     InputType input = static_cast<InputType>(value);
@@ -20,12 +21,12 @@ void test(std::vector<double> const& values) {
     ResultType result = XtdFunc(input);
     // compare the result with the std reference
     ResultType reference = StdFunc(input);
-    CHECK_THAT(result, Catch::Matchers::WithinULP(reference, 1));
+    compare(result, reference);
   }
 }
 
 template <typename ResultType, typename InputType, ResultType (*XtdFunc)(InputType), ResultType (*StdFunc)(float)>
-void test_f(std::vector<double> const& values) {
+inline void test_f(std::vector<double> const& values) {
   for (double value : values) {
     // convert the input data to the type to be tested
     InputType input = static_cast<InputType>(value);
@@ -33,6 +34,6 @@ void test_f(std::vector<double> const& values) {
     ResultType result = XtdFunc(input);
     // compare the result with the std reference
     ResultType reference = StdFunc(static_cast<float>(input));
-    CHECK_THAT(result, Catch::Matchers::WithinULP(reference, 1));
+    compare(result, reference);
   }
 }
