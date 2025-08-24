0d4691d995bddea266c90ff67a2485f80e04aae8
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:03:19 +0200

    Reimplement xtd::log and implement a unit test

--- include/xtd/math/log.h
+++ include/xtd/math/log.h
@@ -1,57 +1,67 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
 #include <concepts>
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
-  XTD_DEVICE_FUNCTION inline constexpr float log(float x) {
+  /* Computes the natural logarithm of arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float log(float arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::log(x);
+    return ::logf(arg);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::log(x);
+    return ::logf(arg);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
-    return sycl::log(x);
+    return sycl::log(arg);
 #else
-    // standard C++ code
-    return std::log(x);
+    // standard C/C++ code
+    return ::logf(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION inline constexpr double log(double x) {
+  /* Computes the natural logarithm of arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double log(double arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::log(x);
+    return ::log(arg);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::log(x);
+    return ::log(arg);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
-    return sycl::log(x);
+    return sycl::log(arg);
 #else
-    // standard C++ code
-    return std::log(x);
+    // standard C/C++ code
+    return ::log(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION inline constexpr float logf(float x) { return log(x); }
+  /* Computes the natural logarithm of arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double log(std::integral auto arg) {
+    return xtd::log(static_cast<double>(arg));
+  }
 
-  template <std::integral T>
-  XTD_DEVICE_FUNCTION inline constexpr double log(T x) {
-    return log(static_cast<double>(x));
+  /* Computes the natural logarithm of arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float logf(std::floating_point auto arg) {
+    return xtd::log(static_cast<float>(arg));
+  }
+  XTD_DEVICE_FUNCTION inline constexpr float logf(std::integral auto arg) {
+    return xtd::log(static_cast<float>(arg));
   }
 
 }  // namespace xtd
