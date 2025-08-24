31669d93b9baf26ebf356f5ac63e44c13b54bc22
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:03:18 +0200

    Reimplement xtd::cbrt and implement a unit test

--- include/xtd/math/cbrt.h
+++ include/xtd/math/cbrt.h
@@ -1,57 +1,67 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
 #include <concepts>
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
-  XTD_DEVICE_FUNCTION inline constexpr float cbrt(float x) {
+  /* Computes the cubic root of arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float cbrt(float arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::cbrt(x);
+    return ::cbrtf(arg);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::cbrt(x);
+    return ::cbrtf(arg);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
-    return sycl::cbrt(x);
+    return sycl::cbrt(arg);
 #else
-    // standard C++ code
-    return std::cbrt(x);
+    // standard C/C++ code
+    return ::cbrtf(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION inline constexpr double cbrt(double x) {
+  /* Computes the cubic root of arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double cbrt(double arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::cbrt(x);
+    return ::cbrt(arg);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::cbrt(x);
+    return ::cbrt(arg);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
-    return sycl::cbrt(x);
+    return sycl::cbrt(arg);
 #else
-    // standard C++ code
-    return std::cbrt(x);
+    // standard C/C++ code
+    return ::cbrt(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION inline constexpr float cbrtf(float x) { return cbrt(x); }
+  /* Computes the cubic root of arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double cbrt(std::integral auto arg) {
+    return xtd::cbrt(static_cast<double>(arg));
+  }
 
-  template <std::integral T>
-  XTD_DEVICE_FUNCTION inline constexpr double cbrt(T x) {
-    return cbrt(static_cast<double>(x));
+  /* Computes the cubic root of arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float cbrtf(std::floating_point auto arg) {
+    return xtd::cbrt(static_cast<float>(arg));
+  }
+  XTD_DEVICE_FUNCTION inline constexpr float cbrtf(std::integral auto arg) {
+    return xtd::cbrt(static_cast<float>(arg));
   }
 
 }  // namespace xtd
---
+++ test/cbrt/cbrt_t.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/cbrt.h"
+
+// test headers
+#include "common/cpu_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 1;
+constexpr int ulps_double = 4;
+
+TEST_CASE("xtd::cbrt", "[cbrt][cpu]") {
+  std::vector<double> values = generate_input_values();
+
+  SECTION("float xtd::cbrt(float)") {
+    test<float, float, xtd::cbrt, mpfr::cbrt>(values, ulps_float);
+  }
+
+  SECTION("double xtd::cbrt(double)") {
+    test<double, double, xtd::cbrt, mpfr::cbrt>(values, ulps_double);
+  }
+
+  SECTION("double xtd::cbrt(int)") {
+    test<double, int, xtd::cbrt, mpfr::cbrt>(values, ulps_double);
+  }
+
+  SECTION("float xtd::cbrtf(float)") {
+    test_f<float, float, xtd::cbrtf, mpfr::cbrt>(values, ulps_float);
+  }
+
+  SECTION("float xtd::cbrtf(double)") {
+    test_f<float, double, xtd::cbrtf, mpfr::cbrt>(values, ulps_float);
+  }
+
+  SECTION("float xtd::cbrtf(int)") {
+    test_f<float, int, xtd::cbrtf, mpfr::cbrt>(values, ulps_float);
+  }
+}
