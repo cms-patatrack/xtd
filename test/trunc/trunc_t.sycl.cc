f52eaa0615e094199bf62cec528d4a5885ea2a7e
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:06:40 +0200

    Reimplement xtd::trunc and implement a unit test

---
+++ test/trunc/trunc_t.sycl.cc
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// SYCL headers
+#include <sycl/sycl.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/trunc.h"
+
+// test headers
+#include "common/sycl_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 0;
+constexpr int ulps_double = 0;
+
+TEST_CASE("xtd::trunc", "[trunc][sycl]") {
+  std::vector<double> values = generate_input_values();
+
+  for (const auto &platform : sycl::platform::get_platforms()) {
+    SECTION(platform.get_info<sycl::info::platform::name>()) {
+      for (const auto &device : platform.get_devices()) {
+        SECTION(device.get_info<sycl::info::device::name>()) {
+          try {
+            sycl::queue queue{device, sycl::property::queue::in_order()};
+
+            SECTION("float xtd::trunc(float)") {
+              test<float, float, xtd::trunc, mpfr::trunc>(queue, values, ulps_float);
+            }
+
+            SECTION("double xtd::trunc(double)") {
+              test<double, double, xtd::trunc, mpfr::trunc>(queue, values, ulps_double);
+            }
+
+            SECTION("double xtd::trunc(int)") {
+              test<double, int, xtd::trunc, mpfr::trunc>(queue, values, ulps_double);
+            }
+
+            SECTION("float xtd::truncf(float)") {
+              test_f<float, float, xtd::truncf, mpfr::trunc>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::truncf(double)") {
+              test_f<float, double, xtd::truncf, mpfr::trunc>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::truncf(int)") {
+              test_f<float, int, xtd::truncf, mpfr::trunc>(queue, values, ulps_float);
+            }
+
+          } catch (sycl::exception const &e) {
+            std::cerr << "SYCL exception:\n"
+                      << e.what() << "\ncaught while running on platform "
+                      << platform.get_info<sycl::info::platform::name>() << ", device "
+                      << device.get_info<sycl::info::device::name>() << '\n';
+            continue;
+          }
+        }
+      }
+    }
+  }
+}
