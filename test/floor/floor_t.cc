8e7a10e7384845186d9a1c6c99fe3b4192b0c3e1
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:03:19 +0200

    Reimplement xtd::floor and implement a unit test

---
+++ test/floor/floor_t.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/floor.h"
+
+// test headers
+#include "common/cpu_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 0;
+constexpr int ulps_double = 0;
+
+TEST_CASE("xtd::floor", "[floor][cpu]") {
+  std::vector<double> values = generate_input_values();
+
+  SECTION("float xtd::floor(float)") {
+    test<float, float, xtd::floor, mpfr::floor>(values, ulps_float);
+  }
+
+  SECTION("double xtd::floor(double)") {
+    test<double, double, xtd::floor, mpfr::floor>(values, ulps_double);
+  }
+
+  SECTION("double xtd::floor(int)") {
+    test<double, int, xtd::floor, mpfr::floor>(values, ulps_double);
+  }
+
+  SECTION("float xtd::floorf(float)") {
+    test_f<float, float, xtd::floorf, mpfr::floor>(values, ulps_float);
+  }
+
+  SECTION("float xtd::floorf(double)") {
+    test_f<float, double, xtd::floorf, mpfr::floor>(values, ulps_float);
+  }
+
+  SECTION("float xtd::floorf(int)") {
+    test_f<float, int, xtd::floorf, mpfr::floor>(values, ulps_float);
+  }
+}
---
+++ test/floor/floor_t.sycl.cc
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// SYCL headers
+#include <sycl/sycl.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/floor.h"
+
+// test headers
+#include "common/sycl_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 0;
+constexpr int ulps_double = 0;
+
+TEST_CASE("xtd::floor", "[floor][sycl]") {
+  std::vector<double> values = generate_input_values();
+
+  for (const auto &platform : sycl::platform::get_platforms()) {
+    SECTION(platform.get_info<sycl::info::platform::name>()) {
+      for (const auto &device : platform.get_devices()) {
+        SECTION(device.get_info<sycl::info::device::name>()) {
+          try {
+            sycl::queue queue{device, sycl::property::queue::in_order()};
+
+            SECTION("float xtd::floor(float)") {
+              test<float, float, xtd::floor, mpfr::floor>(queue, values, ulps_float);
+            }
+
+            SECTION("double xtd::floor(double)") {
+              test<double, double, xtd::floor, mpfr::floor>(queue, values, ulps_double);
+            }
+
+            SECTION("double xtd::floor(int)") {
+              test<double, int, xtd::floor, mpfr::floor>(queue, values, ulps_double);
+            }
+
+            SECTION("float xtd::floorf(float)") {
+              test_f<float, float, xtd::floorf, mpfr::floor>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::floorf(double)") {
+              test_f<float, double, xtd::floorf, mpfr::floor>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::floorf(int)") {
+              test_f<float, int, xtd::floorf, mpfr::floor>(queue, values, ulps_float);
+            }
+
+          } catch (sycl::exception const &e) {
+            std::cerr << "SYCL exception:\n"
+                      << e.what() << "\ncaught while running on platform "
+                      << platform.get_info<sycl::info::platform::name>() << ", device "
+                      << device.get_info<sycl::info::device::name>() << '\n';
+            continue;
+          }
+        }
+      }
+    }
+  }
+}
