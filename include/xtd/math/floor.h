911de7d990f95d9e030c22f83e93e0c01c84ba68
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:03:19 +0200

    Reimplement xtd::floor and implement a unit test

--- include/xtd/math/floor.h
+++ include/xtd/math/floor.h
@@ -1,57 +1,67 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
 #include <concepts>
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
-  XTD_DEVICE_FUNCTION inline constexpr float floor(float x) {
+  /* Computes the largest integral value that is not greater than arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float floor(float arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::floor(x);
+    return ::floorf(arg);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::floor(x);
+    return ::floorf(arg);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
-    return sycl::floor(x);
+    return sycl::floor(arg);
 #else
-    // standard C++ code
-    return std::floor(x);
+    // standard C/C++ code
+    return ::floorf(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION inline constexpr double floor(double x) {
+  /* Computes the largest integral value that is not greater than arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double floor(double arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::floor(x);
+    return ::floor(arg);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::floor(x);
+    return ::floor(arg);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
-    return sycl::floor(x);
+    return sycl::floor(arg);
 #else
-    // standard C++ code
-    return std::floor(x);
+    // standard C/C++ code
+    return ::floor(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION inline constexpr float floorf(float x) { return floor(x); }
+  /* Computes the largest integral value that is not greater than arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double floor(std::integral auto arg) {
+    return xtd::floor(static_cast<double>(arg));
+  }
 
-  template <std::integral T>
-  XTD_DEVICE_FUNCTION inline constexpr double floor(T x) {
-    return floor(static_cast<double>(x));
+  /* Computes the largest integral value that is not greater than arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float floorf(std::floating_point auto arg) {
+    return xtd::floor(static_cast<float>(arg));
+  }
+  XTD_DEVICE_FUNCTION inline constexpr float floorf(std::integral auto arg) {
+    return xtd::floor(static_cast<float>(arg));
   }
 
 }  // namespace xtd
