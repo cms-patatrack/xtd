788cdf402a86519f07835c569244c73e7f116acd
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:03:19 +0200

    Reimplement xtd::exp2 and implement a unit test

---
+++ test/exp2/exp2_t.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/exp2.h"
+
+// test headers
+#include "common/cpu_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 1;
+constexpr int ulps_double = 1;
+
+TEST_CASE("xtd::exp2", "[exp2][cpu]") {
+  std::vector<double> values = generate_input_values();
+
+  SECTION("float xtd::exp2(float)") {
+    test<float, float, xtd::exp2, mpfr::exp2>(values, ulps_float);
+  }
+
+  SECTION("double xtd::exp2(double)") {
+    test<double, double, xtd::exp2, mpfr::exp2>(values, ulps_double);
+  }
+
+  SECTION("double xtd::exp2(int)") {
+    test<double, int, xtd::exp2, mpfr::exp2>(values, ulps_double);
+  }
+
+  SECTION("float xtd::exp2f(float)") {
+    test_f<float, float, xtd::exp2f, mpfr::exp2>(values, ulps_float);
+  }
+
+  SECTION("float xtd::exp2f(double)") {
+    test_f<float, double, xtd::exp2f, mpfr::exp2>(values, ulps_float);
+  }
+
+  SECTION("float xtd::exp2f(int)") {
+    test_f<float, int, xtd::exp2f, mpfr::exp2>(values, ulps_float);
+  }
+}
---
+++ test/exp2/exp2_t.cu
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std::literals;
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// CUDA headers
+#include <cuda_runtime.h>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/exp2.h"
+
+// test headers
+#include "common/cuda_check.h"
+#include "common/cuda_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 2;
+constexpr int ulps_double = 1;
+
+TEST_CASE("xtd::exp2", "[exp2][cuda]") {
+  std::vector<double> values = generate_input_values();
+
+  int deviceCount;
+  cudaError_t cudaStatus = cudaGetDeviceCount(&deviceCount);
+
+  if (cudaStatus != cudaSuccess || deviceCount == 0) {
+    std::cout << "No NVIDIA GPUs found, the test will be skipped.\n\n";
+    exit(EXIT_SUCCESS);
+  }
+
+  for (int device = 0; device < deviceCount; ++device) {
+    cudaDeviceProp properties;
+    CUDA_CHECK(cudaGetDeviceProperties(&properties, device));
+    std::string section = "CUDA GPU "s + std::to_string(device) + ": "s + properties.name;
+    SECTION(section) {
+      // set the current GPU
+      CUDA_CHECK(cudaSetDevice(device));
+
+      // create a CUDA stream for all the asynchronous operations on this GPU
+      cudaStream_t queue;
+      CUDA_CHECK(cudaStreamCreate(&queue));
+
+      SECTION("float xtd::exp2(float)") {
+        test<float, float, xtd::exp2, mpfr::exp2>(queue, values, ulps_float);
+      }
+
+      SECTION("double xtd::exp2(double)") {
+        test<double, double, xtd::exp2, mpfr::exp2>(queue, values, ulps_double);
+      }
+
+      SECTION("double xtd::exp2(int)") {
+        test<double, int, xtd::exp2, mpfr::exp2>(queue, values, ulps_double);
+      }
+
+      SECTION("float xtd::exp2f(float)") {
+        test_f<float, float, xtd::exp2f, mpfr::exp2>(queue, values, ulps_float);
+      }
+
+      SECTION("float xtd::exp2f(double)") {
+        test_f<float, double, xtd::exp2f, mpfr::exp2>(queue, values, ulps_float);
+      }
+
+      SECTION("float xtd::exp2f(int)") {
+        test_f<float, int, xtd::exp2f, mpfr::exp2>(queue, values, ulps_float);
+      }
+
+      CUDA_CHECK(cudaStreamDestroy(queue));
+    }
+  }
+}
