57d1673650aa715f5996a147a7c593fd52b05785
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:03:19 +0200

    Reimplement xtd::ceil and implement a unit test

---
+++ test/ceil/ceil_t.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/ceil.h"
+
+// test headers
+#include "common/cpu_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 0;
+constexpr int ulps_double = 0;
+
+TEST_CASE("xtd::ceil", "[ceil][cpu]") {
+  std::vector<double> values = generate_input_values();
+
+  SECTION("float xtd::ceil(float)") {
+    test<float, float, xtd::ceil, mpfr::ceil>(values, ulps_float);
+  }
+
+  SECTION("double xtd::ceil(double)") {
+    test<double, double, xtd::ceil, mpfr::ceil>(values, ulps_double);
+  }
+
+  SECTION("double xtd::ceil(int)") {
+    test<double, int, xtd::ceil, mpfr::ceil>(values, ulps_double);
+  }
+
+  SECTION("float xtd::ceilf(float)") {
+    test_f<float, float, xtd::ceilf, mpfr::ceil>(values, ulps_float);
+  }
+
+  SECTION("float xtd::ceilf(double)") {
+    test_f<float, double, xtd::ceilf, mpfr::ceil>(values, ulps_float);
+  }
+
+  SECTION("float xtd::ceilf(int)") {
+    test_f<float, int, xtd::ceilf, mpfr::ceil>(values, ulps_float);
+  }
+}
---
+++ test/ceil/ceil_t.hip.cc
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std::literals;
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// HIP headers
+#include <hip/hip_runtime.h>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/ceil.h"
+
+// test headers
+#include "common/hip_check.h"
+#include "common/hip_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 0;
+constexpr int ulps_double = 0;
+
+TEST_CASE("xtd::ceil", "[ceil][hip]") {
+  std::vector<double> values = generate_input_values();
+
+  int deviceCount;
+  hipError_t hipStatus = hipGetDeviceCount(&deviceCount);
+
+  if (hipStatus != hipSuccess || deviceCount == 0) {
+    std::cout << "No AMD GPUs found, the test will be skipped.\n\n";
+    exit(EXIT_SUCCESS);
+  }
+
+  for (int device = 0; device < deviceCount; ++device) {
+    hipDeviceProp_t properties;
+    HIP_CHECK(hipGetDeviceProperties(&properties, device));
+    std::string section = "HIP GPU "s + std::to_string(device) + ": "s + properties.name;
+    SECTION(section) {
+      // set the current GPU
+      HIP_CHECK(hipSetDevice(device));
+
+      // create a HIP stream for all the asynchronous operations on this GPU
+      hipStream_t queue;
+      HIP_CHECK(hipStreamCreate(&queue));
+
+      SECTION("float xtd::ceil(float)") {
+        test<float, float, xtd::ceil, mpfr::ceil>(queue, values, ulps_float);
+      }
+
+      SECTION("double xtd::ceil(double)") {
+        test<double, double, xtd::ceil, mpfr::ceil>(queue, values, ulps_double);
+      }
+
+      SECTION("double xtd::ceil(int)") {
+        test<double, int, xtd::ceil, mpfr::ceil>(queue, values, ulps_double);
+      }
+
+      SECTION("float xtd::ceilf(float)") {
+        test_f<float, float, xtd::ceilf, mpfr::ceil>(queue, values, ulps_float);
+      }
+
+      SECTION("float xtd::ceilf(double)") {
+        test_f<float, double, xtd::ceilf, mpfr::ceil>(queue, values, ulps_float);
+      }
+
+      SECTION("float xtd::ceilf(int)") {
+        test_f<float, int, xtd::ceilf, mpfr::ceil>(queue, values, ulps_float);
+      }
+
+      HIP_CHECK(hipStreamDestroy(queue));
+    }
+  }
+}
