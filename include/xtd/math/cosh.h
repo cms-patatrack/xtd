6579d370b3be1954ebaf92908d345199b48f771a
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:03:19 +0200

    Reimplement xtd::cosh and implement a unit test

--- include/xtd/math/cosh.h
+++ include/xtd/math/cosh.h
@@ -1,22 +1,39 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
 #include <concepts>
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr float cosh(float arg) {
+  /* Computes the hyperbolic cosine of arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float cosh(float arg) {
+#if defined(XTD_TARGET_CUDA)
+    // CUDA device code
+    return ::coshf(arg);
+#elif defined(XTD_TARGET_HIP)
+    // HIP/ROCm device code
+    return ::coshf(arg);
+#elif defined(XTD_TARGET_SYCL)
+    // SYCL device code
+    return sycl::cosh(arg);
+#else
+    // standard C/C++ code
+    return ::coshf(arg);
+#endif
+  }
+
+  /* Computes the hyperbolic cosine of arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double cosh(double arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
     return ::cosh(arg);
@@ -27,34 +44,24 @@ namespace xtd {
     // SYCL device code
     return sycl::cosh(arg);
 #else
-    // standard C++ code
-    return std::cosh(arg);
+    // standard C/C++ code
+    return ::cosh(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr double cosh(double arg) {
-#if defined(XTD_TARGET_CUDA)
-    // CUDA device code
-    return ::cosh(arg);
-#elif defined(XTD_TARGET_HIP)
-    // HIP/ROCm device code
-    return ::cosh(arg);
-#elif defined(XTD_TARGET_SYCL)
-    // SYCL device code
-    return sycl::cosh(arg);
-#else
-    // standard C++ code
-    return std::cosh(arg);
-#endif
+  /* Computes the hyperbolic cosine of arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double cosh(std::integral auto arg) {
+    return xtd::cosh(static_cast<double>(arg));
   }
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr float coshf(float arg) { return cosh(arg); }
-
-  template <std::integral T>
-  XTD_DEVICE_FUNCTION inline constexpr double cosh(T arg) {
-    return cosh(static_cast<double>(arg));
+  /* Computes the hyperbolic cosine of arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float coshf(std::floating_point auto arg) {
+    return xtd::cosh(static_cast<float>(arg));
+  }
+  XTD_DEVICE_FUNCTION inline constexpr float coshf(std::integral auto arg) {
+    return xtd::cosh(static_cast<float>(arg));
   }
 
 }  // namespace xtd
