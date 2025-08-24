6d60a765c6e788e720e6b19e54d786dc0ede1857
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-24 23:03:19 +0200

    Reimplement xtd::cos and implement a unit test

--- include/xtd/math/cos.h
+++ include/xtd/math/cos.h
@@ -1,22 +1,39 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
 #include <concepts>
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr float cos(float arg) {
+  /* Computes the cosine of arg (measured in radians), in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float cos(float arg) {
+#if defined(XTD_TARGET_CUDA)
+    // CUDA device code
+    return ::cosf(arg);
+#elif defined(XTD_TARGET_HIP)
+    // HIP/ROCm device code
+    return ::cosf(arg);
+#elif defined(XTD_TARGET_SYCL)
+    // SYCL device code
+    return sycl::cos(arg);
+#else
+    // standard C/C++ code
+    return ::cosf(arg);
+#endif
+  }
+
+  /* Computes the cosine of arg (measured in radians), in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double cos(double arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
     return ::cos(arg);
@@ -27,34 +44,24 @@ namespace xtd {
     // SYCL device code
     return sycl::cos(arg);
 #else
-    // standard C++ code
-    return std::cos(arg);
+    // standard C/C++ code
+    return ::cos(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr double cos(double arg) {
-#if defined(XTD_TARGET_CUDA)
-    // CUDA device code
-    return ::cos(arg);
-#elif defined(XTD_TARGET_HIP)
-    // HIP/ROCm device code
-    return ::cos(arg);
-#elif defined(XTD_TARGET_SYCL)
-    // SYCL device code
-    return sycl::cos(arg);
-#else
-    // standard C++ code
-    return std::cos(arg);
-#endif
+  /* Computes the cosine of arg (measured in radians), in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double cos(std::integral auto arg) {
+    return xtd::cos(static_cast<double>(arg));
   }
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr float cosf(float arg) { return cos(arg); }
-
-  template <std::integral T>
-  XTD_DEVICE_FUNCTION inline constexpr double cos(T arg) {
-    return cos(static_cast<double>(arg));
+  /* Computes the cosine of arg (measured in radians), in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float cosf(std::floating_point auto arg) {
+    return xtd::cos(static_cast<float>(arg));
+  }
+  XTD_DEVICE_FUNCTION inline constexpr float cosf(std::integral auto arg) {
+    return xtd::cos(static_cast<float>(arg));
   }
 
 }  // namespace xtd
---
+++ test/cos/cos_t.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/cos.h"
+
+// test headers
+#include "common/cpu_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 1;
+constexpr int ulps_double = 1;
+
+TEST_CASE("xtd::cos", "[cos][cpu]") {
+  std::vector<double> values = generate_input_values();
+
+  SECTION("float xtd::cos(float)") {
+    test<float, float, xtd::cos, mpfr::cos>(values, ulps_float);
+  }
+
+  SECTION("double xtd::cos(double)") {
+    test<double, double, xtd::cos, mpfr::cos>(values, ulps_double);
+  }
+
+  SECTION("double xtd::cos(int)") {
+    test<double, int, xtd::cos, mpfr::cos>(values, ulps_double);
+  }
+
+  SECTION("float xtd::cosf(float)") {
+    test_f<float, float, xtd::cosf, mpfr::cos>(values, ulps_float);
+  }
+
+  SECTION("float xtd::cosf(double)") {
+    test_f<float, double, xtd::cosf, mpfr::cos>(values, ulps_float);
+  }
+
+  SECTION("float xtd::cosf(int)") {
+    test_f<float, int, xtd::cosf, mpfr::cos>(values, ulps_float);
+  }
+}
