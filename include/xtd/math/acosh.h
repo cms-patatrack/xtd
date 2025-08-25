58630766f9f200cfa731b7527cea054dc400471c
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-25 22:32:01 +0200

    Reimplement xtd::acosh and implement a unit test

--- include/xtd/math/acosh.h
+++ include/xtd/math/acosh.h
@@ -1,22 +1,39 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
 #include <concepts>
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr float acosh(float arg) {
+  /* Computes the inverse hyperbolic cosine of arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float acosh(float arg) {
+#if defined(XTD_TARGET_CUDA)
+    // CUDA device code
+    return ::acoshf(arg);
+#elif defined(XTD_TARGET_HIP)
+    // HIP/ROCm device code
+    return ::acoshf(arg);
+#elif defined(XTD_TARGET_SYCL)
+    // SYCL device code
+    return sycl::acosh(arg);
+#else
+    // standard C/C++ code
+    return ::acoshf(arg);
+#endif
+  }
+
+  /* Computes the inverse hyperbolic cosine of arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double acosh(double arg) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
     return ::acosh(arg);
@@ -27,34 +44,24 @@ namespace xtd {
     // SYCL device code
     return sycl::acosh(arg);
 #else
-    // standard C++ code
-    return std::acosh(arg);
+    // standard C/C++ code
+    return ::acosh(arg);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr double acosh(double arg) {
-#if defined(XTD_TARGET_CUDA)
-    // CUDA device code
-    return ::acosh(arg);
-#elif defined(XTD_TARGET_HIP)
-    // HIP/ROCm device code
-    return ::acosh(arg);
-#elif defined(XTD_TARGET_SYCL)
-    // SYCL device code
-    return sycl::acosh(arg);
-#else
-    // standard C++ code
-    return std::acosh(arg);
-#endif
+  /* Computes the inverse hyperbolic cosine of arg, in double precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double acosh(std::integral auto arg) {
+    return xtd::acosh(static_cast<double>(arg));
   }
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr float acoshf(float arg) { return acosh(arg); }
-
-  template <std::integral T>
-  XTD_DEVICE_FUNCTION inline constexpr double acosh(T arg) {
-    return acosh(static_cast<double>(arg));
+  /* Computes the inverse hyperbolic cosine of arg, in single precision.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float acoshf(std::floating_point auto arg) {
+    return xtd::acosh(static_cast<float>(arg));
+  }
+  XTD_DEVICE_FUNCTION inline constexpr float acoshf(std::integral auto arg) {
+    return xtd::acosh(static_cast<float>(arg));
   }
 
 }  // namespace xtd
---
+++ test/acosh/acosh_t.sycl.cc
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// SYCL headers
+#include <sycl/sycl.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/acosh.h"
+
+// test headers
+#include "common/sycl_test.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 2;
+constexpr int ulps_double = 2;
+
+TEST_CASE("xtd::acosh", "[acosh][sycl]") {
+  std::vector<double> values = generate_input_values();
+
+  for (const auto &platform : sycl::platform::get_platforms()) {
+    SECTION(platform.get_info<sycl::info::platform::name>()) {
+      for (const auto &device : platform.get_devices()) {
+        SECTION(device.get_info<sycl::info::device::name>()) {
+          try {
+            sycl::queue queue{device, sycl::property::queue::in_order()};
+
+            SECTION("float xtd::acosh(float)") {
+              test<float, float, xtd::acosh, mpfr::acosh>(queue, values, ulps_float);
+            }
+
+            SECTION("double xtd::acosh(double)") {
+              test<double, double, xtd::acosh, mpfr::acosh>(queue, values, ulps_double);
+            }
+
+            SECTION("double xtd::acosh(int)") {
+              test<double, int, xtd::acosh, mpfr::acosh>(queue, values, ulps_double);
+            }
+
+            SECTION("float xtd::acoshf(float)") {
+              test_f<float, float, xtd::acoshf, mpfr::acosh>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::acoshf(double)") {
+              test_f<float, double, xtd::acoshf, mpfr::acosh>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::acoshf(int)") {
+              test_f<float, int, xtd::acoshf, mpfr::acosh>(queue, values, ulps_float);
+            }
+
+          } catch (sycl::exception const &e) {
+            std::cerr << "SYCL exception:\n"
+                      << e.what() << "\ncaught while running on platform "
+                      << platform.get_info<sycl::info::platform::name>() << ", device "
+                      << device.get_info<sycl::info::device::name>() << '\n';
+            continue;
+          }
+        }
+      }
+    }
+  }
+}
