a966e00b25653fa5e61ca25e52fc424bbe34c97a
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-26 18:39:41 +0200

    Reimplement xtd::atan2 and implement a unit test

--- include/xtd/math/atan2.h
+++ include/xtd/math/atan2.h
@@ -1,60 +1,75 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
 #include <concepts>
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr float atan2(float x, float y) {
+  /* Computes the inverse tangent (measured in radians) or y/x, in single precision,
+   * using the signs of the two arguments to determine the quadrant of the result.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr float atan2(float y, float x) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::atan2(x, y);
+    return ::atan2f(y, x);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::atan2(x, y);
+    return ::atan2f(y, x);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
-    return sycl::atan2(x, y);
+    return sycl::atan2(y, x);
 #else
-    // standard C++ code
-    return std::atan2(x, y);
+    // standard C/C++ code
+    return ::atan2f(y, x);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr double atan2(double x, double y) {
+  /* Computes the inverse tangent (measured in radians) or y/x, in double precision,
+   * using the signs of the two arguments to determine the quadrant of the result.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr double atan2(double y, double x) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::atan2(x, y);
+    return ::atan2(y, x);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::atan2(x, y);
+    return ::atan2(y, x);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
-    return sycl::atan2(x, y);
+    return sycl::atan2(y, x);
 #else
-    // standard C++ code
-    return std::atan2(x, y);
+    // standard C/C++ code
+    return ::atan2(y, x);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION
-  inline constexpr float atan2f(float x, float y) { return atan2(x, y); }
+  /* Computes the inverse tangent (measured in radians) or y/x, in double precision,
+   * using the signs of the two arguments to determine the quadrant of the result.
+   */
+  template <std::integral Integral>
+  XTD_DEVICE_FUNCTION inline constexpr double atan2(Integral y, Integral x) {
+    return xtd::atan2(static_cast<double>(y), static_cast<double>(x));
+  }
 
-  template <std::integral T>
-  XTD_DEVICE_FUNCTION inline constexpr double atan2(T x, T y) {
-    return atan2(static_cast<double>(x), static_cast<double>(y));
+  /* Computes the inverse tangent (measured in radians) or y/x, in single precision,
+   * using the signs of the two arguments to determine the quadrant of the result.
+   */
+  template <std::floating_point FloatingPoint>
+  XTD_DEVICE_FUNCTION inline constexpr float atan2f(FloatingPoint y, FloatingPoint x) {
+    return xtd::atan2(static_cast<float>(y), static_cast<float>(x));
+  }
+
+  template <std::integral Integral>
+  XTD_DEVICE_FUNCTION inline constexpr float atan2f(Integral y, Integral x) {
+    return xtd::atan2(static_cast<float>(y), static_cast<float>(x));
   }
 
 }  // namespace xtd
---
+++ test/atan2/atan2_t.cu
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std::literals;
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// CUDA headers
+#include <cuda_runtime.h>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/atan2.h"
+
+// test headers
+#include "common/cuda_check.h"
+#include "common/cuda_test2.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 2;
+constexpr int ulps_double = 2;
+
+constexpr auto ref_atan2 = [](mpfr_double y, mpfr_double x) -> mpfr_double { return mpfr::atan2(y, x); };
+constexpr auto ref_atan2f = [](mpfr_single y, mpfr_single x) -> mpfr_single { return mpfr::atan2(y, x); };
+
+TEST_CASE("xtd::atan2", "[atan][cuda]") {
+  std::vector<double> values = generate_input_values();
+
+  int deviceCount;
+  cudaError_t cudaStatus = cudaGetDeviceCount(&deviceCount);
+
+  if (cudaStatus != cudaSuccess || deviceCount == 0) {
+    std::cout << "No NVIDIA GPUs found, the test will be skipped.\n\n";
+    exit(EXIT_SUCCESS);
+  }
+
+  for (int device = 0; device < deviceCount; ++device) {
+    cudaDeviceProp properties;
+    CUDA_CHECK(cudaGetDeviceProperties(&properties, device));
+    std::string section = "CUDA GPU "s + std::to_string(device) + ": "s + properties.name;
+    SECTION(section) {
+      // set the current GPU
+      CUDA_CHECK(cudaSetDevice(device));
+
+      // create a CUDA stream for all the asynchronous operations on this GPU
+      cudaStream_t queue;
+      CUDA_CHECK(cudaStreamCreate(&queue));
+
+      SECTION("float xtd::atan2(float, float)") {
+        test_2<float, float, xtd::atan2, ref_atan2>(queue, values, ulps_float);
+      }
+
+      SECTION("double xtd::atan2(double, double)") {
+        test_2<double, double, xtd::atan2, ref_atan2>(queue, values, ulps_double);
+      }
+
+      SECTION("double xtd::atan2(int, int)") {
+        test_2<double, int, xtd::atan2, ref_atan2>(queue, values, ulps_double);
+      }
+
+      SECTION("float xtd::atan2f(float, float)") {
+        test_2f<float, float, xtd::atan2f, ref_atan2f>(queue, values, ulps_float);
+      }
+
+      SECTION("float xtd::atan2f(double, double)") {
+        test_2f<float, double, xtd::atan2f, ref_atan2f>(queue, values, ulps_float);
+      }
+
+      SECTION("float xtd::atan2f(int, int)") {
+        test_2f<float, int, xtd::atan2f, ref_atan2f>(queue, values, ulps_float);
+      }
+
+      CUDA_CHECK(cudaStreamDestroy(queue));
+    }
+  }
+}
