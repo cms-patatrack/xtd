46ec51ee6c20a52a8d72050db58c7aa39042df33
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-27 08:03:08 +0200

    Reimplement xtd::pow and implement a unit test

--- include/xtd/math/pow.h
+++ include/xtd/math/pow.h
@@ -1,35 +1,38 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
+#include <concepts>
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
+  /* Computes the value of base raised to the power exp, in single precision.
+   */
   XTD_DEVICE_FUNCTION inline constexpr float pow(float base, float exp) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::pow(base, exp);
+    return ::powf(base, exp);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::pow(base, exp);
+    return ::powf(base, exp);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
     return sycl::pow(base, exp);
 #else
-    // standard C++ code
-    return std::pow(base, exp);
+    // standard C/C++ code
+    return ::powf(base, exp);
 #endif
   }
 
+  /* Computes the value of base raised to the power exp, in double precision.
+   */
   XTD_DEVICE_FUNCTION inline constexpr double pow(double base, double exp) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
@@ -41,11 +44,28 @@ namespace xtd {
     // SYCL device code
     return sycl::pow(base, exp);
 #else
-    // standard C++ code
-    return std::pow(base, exp);
+    // standard C/C++ code
+    return ::pow(base, exp);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION inline constexpr float powf(float base, float exp) { return pow(base, exp); }
+  /* Computes the value of base raised to the power exp, in double precision.
+   */
+  template <std::integral Integral>
+  XTD_DEVICE_FUNCTION inline constexpr double pow(Integral base, Integral exp) {
+    return xtd::pow(static_cast<double>(base), static_cast<double>(exp));
+  }
+
+  /* Computes the value of base raised to the power exp, in single precision.
+   */
+  template <std::floating_point FloatingPoint>
+  XTD_DEVICE_FUNCTION inline constexpr float powf(FloatingPoint base, FloatingPoint exp) {
+    return xtd::pow(static_cast<float>(base), static_cast<float>(exp));
+  }
+
+  template <std::integral Integral>
+  XTD_DEVICE_FUNCTION inline constexpr float powf(Integral base, Integral exp) {
+    return xtd::pow(static_cast<float>(base), static_cast<float>(exp));
+  }
 
 }  // namespace xtd
---
+++ test/pow/pow_t.sycl.cc
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// SYCL headers
+#include <sycl/sycl.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/pow.h"
+
+// test headers
+#include "common/sycl_test2.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 2;
+constexpr int ulps_double = 1;
+
+constexpr auto ref_pow = [](mpfr_double y, mpfr_double x) { return mpfr::pow(y, x); };
+constexpr auto ref_powf = [](mpfr_single y, mpfr_single x) { return mpfr::pow(y, x); };
+
+TEST_CASE("xtd::pow", "[pow][sycl]") {
+  std::vector<double> values = generate_input_values();
+
+  for (const auto &platform : sycl::platform::get_platforms()) {
+    SECTION(platform.get_info<sycl::info::platform::name>()) {
+      for (const auto &device : platform.get_devices()) {
+        SECTION(device.get_info<sycl::info::device::name>()) {
+          try {
+            sycl::queue queue{device, sycl::property::queue::in_order()};
+
+            SECTION("float xtd::pow(float, float)") {
+              test_2<float, float, xtd::pow, ref_pow>(queue, values, ulps_float);
+            }
+
+            SECTION("double xtd::pow(double, double)") {
+              test_2<double, double, xtd::pow, ref_pow>(queue, values, ulps_double);
+            }
+
+            SECTION("double xtd::pow(int, int)") {
+              test_2<double, int, xtd::pow, ref_pow>(queue, values, ulps_double);
+            }
+
+            SECTION("float xtd::powf(float, float)") {
+              test_2f<float, float, xtd::powf, ref_powf>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::powf(double, double)") {
+              test_2f<float, double, xtd::powf, ref_powf>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::powf(int, int)") {
+              test_2f<float, int, xtd::powf, ref_powf>(queue, values, ulps_float);
+            }
+
+          } catch (sycl::exception const &e) {
+            std::cerr << "SYCL exception:\n"
+                      << e.what() << "\ncaught while running on platform "
+                      << platform.get_info<sycl::info::platform::name>() << ", device "
+                      << device.get_info<sycl::info::device::name>() << '\n';
+            continue;
+          }
+        }
+      }
+    }
+  }
+}
