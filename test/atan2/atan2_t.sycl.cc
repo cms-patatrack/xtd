37120ab49bf1e46ff2f42ff7d3b2e56bc5ec039b
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-27 07:53:40 +0200

    Fix atan2 test name

--- test/atan2/atan2_t.sycl.cc
+++ test/atan2/atan2_t.sycl.cc
@@ -33,7 +33,7 @@ constexpr int ulps_double = 2;
 constexpr auto ref_atan2 = [](mpfr_double y, mpfr_double x) { return mpfr::atan2(y, x); };
 constexpr auto ref_atan2f = [](mpfr_single y, mpfr_single x) { return mpfr::atan2(y, x); };
 
-TEST_CASE("xtd::atan2", "[atan][sycl]") {
+TEST_CASE("xtd::atan2", "[atan2][sycl]") {
   std::vector<double> values = generate_input_values();
 
   for (const auto &platform : sycl::platform::get_platforms()) {
