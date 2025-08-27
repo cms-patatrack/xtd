6f62f517bc3a42f42561be609dd820a21929a49b
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-27 08:03:08 +0200

    Reimplement xtd::pow and implement a unit test

---
+++ test/pow/pow_t.cc
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/pow.h"
+
+// test headers
+#include "common/cpu_test2.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 1;
+constexpr int ulps_double = 1;
+
+constexpr auto ref_pow = [](mpfr_double y, mpfr_double x) { return mpfr::pow(y, x); };
+constexpr auto ref_powf = [](mpfr_single y, mpfr_single x) { return mpfr::pow(y, x); };
+
+TEST_CASE("xtd::pow", "[pow][cpu]") {
+  std::vector<double> values = generate_input_values();
+
+  SECTION("float xtd::pow(float, float)") {
+    test_2<float, float, xtd::pow, ref_pow>(values, ulps_float);
+  }
+
+  SECTION("double xtd::pow(double, double)") {
+    test_2<double, double, xtd::pow, ref_pow>(values, ulps_double);
+  }
+
+  SECTION("double xtd::pow(int, int)") {
+    test_2<double, int, xtd::pow, ref_pow>(values, ulps_double);
+  }
+
+  SECTION("float xtd::powf(float, float)") {
+    test_2f<float, float, xtd::powf, ref_powf>(values, ulps_float);
+  }
+
+  SECTION("float xtd::powf(double, double)") {
+    test_2f<float, double, xtd::powf, ref_powf>(values, ulps_float);
+  }
+
+  SECTION("float xtd::powf(int, int)") {
+    test_2f<float, int, xtd::powf, ref_powf>(values, ulps_float);
+  }
+}
---
+++ test/pow/pow_t.hip.cc
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std::literals;
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// HIP headers
+#include <hip/hip_runtime.h>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/pow.h"
+
+// test headers
+#include "common/hip_check.h"
+#include "common/hip_test2.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 1;
+constexpr int ulps_double = 1;
+
+constexpr auto ref_pow = [](mpfr_double y, mpfr_double x) { return mpfr::pow(y, x); };
+constexpr auto ref_powf = [](mpfr_single y, mpfr_single x) { return mpfr::pow(y, x); };
+
+TEST_CASE("xtd::pow", "[pow][hip]") {
+  std::vector<double> values = generate_input_values();
+
+  int deviceCount;
+  hipError_t hipStatus = hipGetDeviceCount(&deviceCount);
+
+  if (hipStatus != hipSuccess || deviceCount == 0) {
+    std::cout << "No AMD GPUs found, the test will be skipped.\n\n";
+    exit(EXIT_SUCCESS);
+  }
+
+  for (int device = 0; device < deviceCount; ++device) {
+    hipDeviceProp_t properties;
+    HIP_CHECK(hipGetDeviceProperties(&properties, device));
+    std::string section = "HIP GPU "s + std::to_string(device) + ": "s + properties.name;
+    SECTION(section) {
+      // set the current GPU
+      HIP_CHECK(hipSetDevice(device));
+
+      // create a HIP stream for all the asynchronous operations on this GPU
+      hipStream_t queue;
+      HIP_CHECK(hipStreamCreate(&queue));
+
+      SECTION("float xtd::pow(float, float)") {
+        test_2<float, float, xtd::pow, ref_pow>(queue, values, ulps_float);
+      }
+
+      SECTION("double xtd::pow(double, double)") {
+        test_2<double, double, xtd::pow, ref_pow>(queue, values, ulps_double);
+      }
+
+      SECTION("double xtd::pow(int, int)") {
+        test_2<double, int, xtd::pow, ref_pow>(queue, values, ulps_double);
+      }
+
+      SECTION("float xtd::powf(float, float)") {
+        test_2f<float, float, xtd::powf, ref_powf>(queue, values, ulps_float);
+      }
+
+      SECTION("float xtd::powf(double, double)") {
+        test_2f<float, double, xtd::powf, ref_powf>(queue, values, ulps_float);
+      }
+
+      SECTION("float xtd::powf(int, int)") {
+        test_2f<float, int, xtd::powf, ref_powf>(queue, values, ulps_float);
+      }
+
+      HIP_CHECK(hipStreamDestroy(queue));
+    }
+  }
+}
