30a21e6bbaeff7e9b45b782a9b8f400c5476ef9f
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-27 08:05:38 +0200

    Implement xtd::fmax and implement a unit test

---
+++ test/fmax/fmax_t.cc
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/fmax.h"
+
+// test headers
+#include "common/cpu_test2.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 0;
+constexpr int ulps_double = 0;
+
+constexpr auto ref_fmax = [](mpfr_double y, mpfr_double x) { return mpfr::fmax(y, x); };
+constexpr auto ref_fmaxf = [](mpfr_single y, mpfr_single x) { return mpfr::fmax(y, x); };
+
+TEST_CASE("xtd::fmax", "[fmax][cpu]") {
+  std::vector<double> values = generate_input_values();
+
+  SECTION("float xtd::fmax(float, float)") {
+    test_2<float, float, xtd::fmax, ref_fmax>(values, ulps_float);
+  }
+
+  SECTION("double xtd::fmax(double, double)") {
+    test_2<double, double, xtd::fmax, ref_fmax>(values, ulps_double);
+  }
+
+  SECTION("double xtd::fmax(int, int)") {
+    test_2<double, int, xtd::fmax, ref_fmax>(values, ulps_double);
+  }
+
+  SECTION("float xtd::fmaxf(float, float)") {
+    test_2f<float, float, xtd::fmaxf, ref_fmaxf>(values, ulps_float);
+  }
+
+  SECTION("float xtd::fmaxf(double, double)") {
+    test_2f<float, double, xtd::fmaxf, ref_fmaxf>(values, ulps_float);
+  }
+
+  SECTION("float xtd::fmaxf(int, int)") {
+    test_2f<float, int, xtd::fmaxf, ref_fmaxf>(values, ulps_float);
+  }
+}
---
+++ test/fmax/fmax_t.sycl.cc
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// SYCL headers
+#include <sycl/sycl.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/fmax.h"
+
+// test headers
+#include "common/sycl_test2.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 0;
+constexpr int ulps_double = 0;
+
+constexpr auto ref_fmax = [](mpfr_double y, mpfr_double x) { return mpfr::fmax(y, x); };
+constexpr auto ref_fmaxf = [](mpfr_single y, mpfr_single x) { return mpfr::fmax(y, x); };
+
+TEST_CASE("xtd::fmax", "[fmax][sycl]") {
+  std::vector<double> values = generate_input_values();
+
+  for (const auto &platform : sycl::platform::get_platforms()) {
+    SECTION(platform.get_info<sycl::info::platform::name>()) {
+      for (const auto &device : platform.get_devices()) {
+        SECTION(device.get_info<sycl::info::device::name>()) {
+          try {
+            sycl::queue queue{device, sycl::property::queue::in_order()};
+
+            SECTION("float xtd::fmax(float, float)") {
+              test_2<float, float, xtd::fmax, ref_fmax>(queue, values, ulps_float);
+            }
+
+            SECTION("double xtd::fmax(double, double)") {
+              test_2<double, double, xtd::fmax, ref_fmax>(queue, values, ulps_double);
+            }
+
+            SECTION("double xtd::fmax(int, int)") {
+              test_2<double, int, xtd::fmax, ref_fmax>(queue, values, ulps_double);
+            }
+
+            SECTION("float xtd::fmaxf(float, float)") {
+              test_2f<float, float, xtd::fmaxf, ref_fmaxf>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::fmaxf(double, double)") {
+              test_2f<float, double, xtd::fmaxf, ref_fmaxf>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::fmaxf(int, int)") {
+              test_2f<float, int, xtd::fmaxf, ref_fmaxf>(queue, values, ulps_float);
+            }
+
+          } catch (sycl::exception const &e) {
+            std::cerr << "SYCL exception:\n"
+                      << e.what() << "\ncaught while running on platform "
+                      << platform.get_info<sycl::info::platform::name>() << ", device "
+                      << device.get_info<sycl::info::device::name>() << '\n';
+            continue;
+          }
+        }
+      }
+    }
+  }
+}
