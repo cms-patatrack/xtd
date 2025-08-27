96bbcde28a1ec5329669634a7ba18c88b298583f
Andrea Bocci <andrea.bocci@cern.ch>
2025-08-27 08:03:08 +0200

    Reimplement xtd::hypot and implement a unit test

--- include/xtd/math/hypot.h
+++ include/xtd/math/hypot.h
@@ -1,35 +1,40 @@
 /*
  * Copyright 2025 European Organization for Nuclear Research (CERN)
- * Authors: Simone Balducci <simone.balducci@cern.ch>
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>, Simone Balducci <simone.balducci@cern.ch>
  * SPDX-License-Identifier: MPL-2.0
  */
 
 #pragma once
 
-#include "xtd/internal/defines.h"
-
-#if !defined(XTD_TARGET_CUDA) && !defined(XTD_TARGET_HIP) && !defined(XTD_TARGET_SYCL)
+#include <concepts>
 #include <cmath>
-#endif
+
+#include "xtd/internal/defines.h"
 
 namespace xtd {
 
+  /* Computes the square root of the sum of the squares of the two arguments, in single precision,
+   * without undue overflow or underflow at intermediate stages of the computation.
+   */
   XTD_DEVICE_FUNCTION inline constexpr float hypot(float x, float y) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
-    return ::hypot(x, y);
+    return ::hypotf(x, y);
 #elif defined(XTD_TARGET_HIP)
     // HIP/ROCm device code
-    return ::hypot(x, y);
+    return ::hypotf(x, y);
 #elif defined(XTD_TARGET_SYCL)
     // SYCL device code
     return sycl::hypot(x, y);
 #else
-    // standard C++ code
-    return std::hypot(x, y);
+    // standard C/C++ code
+    return ::hypotf(x, y);
 #endif
   }
 
+  /* Computes the square root of the sum of the squares of the two arguments, in double precision,
+   * without undue overflow or underflow at intermediate stages of the computation.
+   */
   XTD_DEVICE_FUNCTION inline constexpr double hypot(double x, double y) {
 #if defined(XTD_TARGET_CUDA)
     // CUDA device code
@@ -41,11 +46,30 @@ namespace xtd {
     // SYCL device code
     return sycl::hypot(x, y);
 #else
-    // standard C++ code
-    return std::hypot(x, y);
+    // standard C/C++ code
+    return ::hypot(x, y);
 #endif
   }
 
-  XTD_DEVICE_FUNCTION inline constexpr float hypotf(float x, float y) { return hypot(x, y); }
+  /* Computes the square root of the sum of the squares of the two arguments, in double precision,
+   * without undue overflow or underflow at intermediate stages of the computation.
+   */
+  template <std::integral Integral>
+  XTD_DEVICE_FUNCTION inline constexpr double hypot(Integral x, Integral y) {
+    return xtd::hypot(static_cast<double>(x), static_cast<double>(y));
+  }
+
+  /* Computes the square root of the sum of the squares of the two arguments, in single precision,
+   * without undue overflow or underflow at intermediate stages of the computation.
+   */
+  template <std::floating_point FloatingPoint>
+  XTD_DEVICE_FUNCTION inline constexpr float hypotf(FloatingPoint x, FloatingPoint y) {
+    return xtd::hypot(static_cast<float>(x), static_cast<float>(y));
+  }
+
+  template <std::integral Integral>
+  XTD_DEVICE_FUNCTION inline constexpr float hypotf(Integral x, Integral y) {
+    return xtd::hypot(static_cast<float>(x), static_cast<float>(y));
+  }
 
 }  // namespace xtd
---
+++ test/hypot/hypot_t.sycl.cc
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// C++ standard headers
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+// Catch2 headers
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_NO_POSIX_SIGNALS
+#include <catch.hpp>
+
+// SYCL headers
+#include <sycl/sycl.hpp>
+
+// mpfr::real headers
+#include <real.hpp>
+
+// xtd headers
+#include "xtd/math/hypot.h"
+
+// test headers
+#include "common/sycl_test2.h"
+#include "common/math_inputs.h"
+
+constexpr int ulps_float = 3;
+constexpr int ulps_double = 2;
+
+constexpr auto ref_hypot = [](mpfr_double y, mpfr_double x) { return mpfr::hypot(y, x); };
+constexpr auto ref_hypotf = [](mpfr_single y, mpfr_single x) { return mpfr::hypot(y, x); };
+
+TEST_CASE("xtd::hypot", "[hypot][sycl]") {
+  std::vector<double> values = generate_input_values();
+
+  for (const auto &platform : sycl::platform::get_platforms()) {
+    SECTION(platform.get_info<sycl::info::platform::name>()) {
+      for (const auto &device : platform.get_devices()) {
+        SECTION(device.get_info<sycl::info::device::name>()) {
+          try {
+            sycl::queue queue{device, sycl::property::queue::in_order()};
+
+            SECTION("float xtd::hypot(float, float)") {
+              test_2<float, float, xtd::hypot, ref_hypot>(queue, values, ulps_float);
+            }
+
+            SECTION("double xtd::hypot(double, double)") {
+              test_2<double, double, xtd::hypot, ref_hypot>(queue, values, ulps_double);
+            }
+
+            SECTION("double xtd::hypot(int, int)") {
+              test_2<double, int, xtd::hypot, ref_hypot>(queue, values, ulps_double);
+            }
+
+            SECTION("float xtd::hypotf(float, float)") {
+              test_2f<float, float, xtd::hypotf, ref_hypotf>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::hypotf(double, double)") {
+              test_2f<float, double, xtd::hypotf, ref_hypotf>(queue, values, ulps_float);
+            }
+
+            SECTION("float xtd::hypotf(int, int)") {
+              test_2f<float, int, xtd::hypotf, ref_hypotf>(queue, values, ulps_float);
+            }
+
+          } catch (sycl::exception const &e) {
+            std::cerr << "SYCL exception:\n"
+                      << e.what() << "\ncaught while running on platform "
+                      << platform.get_info<sycl::info::platform::name>() << ", device "
+                      << device.get_info<sycl::info::device::name>() << '\n';
+            continue;
+          }
+        }
+      }
+    }
+  }
+}
