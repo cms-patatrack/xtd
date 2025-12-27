5cc07778ee7f118095df42567130e45dbe23598e
Andrea Bocci <andrea.bocci@cern.ch>
2025-12-28 00:23:53 +0100

    Implement signbit (#59)

---
+++ include/xtd/math/signbit.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+#pragma once
+
+#include <bit>
+#include <concepts>
+#include <cstdint>
+
+#include "xtd/internal/defines.h"
+
+namespace xtd {
+
+  /* Returns one if the single precision floating-point argument is negative, including -0.0f, -inf or a negative NaN.
+   * Returns zero otherwise.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr int signbit(float arg) {
+    constexpr uint32_t sign_mask = 0x80000000u;
+    uint32_t bits = std::bit_cast<uint32_t>(arg);
+    return (bits & sign_mask) ? 1 : 0;
+  }
+
+  /* Returns one if the double precision floating-point argument is negative, including -0.0, -inf or a negative NaN.
+   * Returns zero otherwise.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr int signbit(double arg) {
+    constexpr uint64_t sign_mask = 0x8000000000000000ull;
+    uint64_t bits = std::bit_cast<uint64_t>(arg);
+    return (bits & sign_mask) ? 1 : 0;
+  }
+
+  /* Returns one if the integer argument is negative, or zero otherwise.
+   */
+  XTD_DEVICE_FUNCTION inline constexpr int signbit(std::signed_integral auto arg) {
+    return arg < 0 ? 1 : 0;
+  }
+
+  XTD_DEVICE_FUNCTION inline constexpr int signbit(std::unsigned_integral auto arg) {
+    return 0;
+  }
+
+}  // namespace xtd
---
+++ test/src/math/signbit/signbit_t.cc
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 European Organization for Nuclear Research (CERN)
+ * Authors: Andrea Bocci <andrea.bocci@cern.ch>, Aurora Perego <aurora.perego@cern.ch>
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// Catch2 headers
+#include <catch.hpp>
+
+// xtd headers
+#include "xtd/math/signbit.h"
+
+// test headers
+#include "common/cpu/device.h"
+#include "common/cpu/validate.h"
+#include "reference_signbit.h"
+
+TEST_CASE("xtd::signbit", "[signbit][cpu]") {
+  const auto& device = test::cpu::device();
+  DYNAMIC_SECTION("CPU: " << device.name()) {
+    SECTION("int xtd::signbit(float)") {
+      validate<int, float, xtd::signbit, reference_signbit>(device);
+    }
+
+    SECTION("int xtd::signbit(double)") {
+      validate<int, double, xtd::signbit, reference_signbit>(device);
+    }
+
+    SECTION("int xtd::signbit(int)") {
+      validate<int, int, xtd::signbit, reference_signbit>(device);
+    }
+
+    SECTION("int xtd::signbit(unsigned int)") {
+      validate<int, unsigned int, xtd::signbit, reference_signbit>(device);
+    }
+  }
+}
